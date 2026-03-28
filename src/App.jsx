f8031934c917e7d546fade86c7aa8de5df30e500
Isaac Gavin <isaac.ciutarno@binus.ac.id>
2026-03-28 11:53:27 +0700

    feat: redirect root path to /verse

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import VersePage from "./pages/VersePage";
 import CharacterPage from "./pages/CharacterPage";
 import "./App.css";
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/verse" replace />} />
         <Route path="/verse" element={<VersePage />} />
         <Route path="/character" element={<CharacterPage />} />
       </Routes>
